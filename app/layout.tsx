e4963c43a55e9ffadbbe8b7a543a4694774b6e7a
Jaivin Phabiani <90526871+JaivinP@users.noreply.github.com>
2026-05-21 18:37:46 -0700

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geist = Geist({
   variable: "--font-geist-var",
